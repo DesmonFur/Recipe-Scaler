7591671657e0ba2dcf9b8e8a9c2e0dab9a20e6e9
desfurdev <desfurdev@gmail.com>
2026-05-19 09:49:56 -0400

    feat: render saved recipes as cards

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,5 +1,22 @@
-import { useState } from "react";
+import type { Recipe } from "../types";
 
-export function RecipeCard() {
-  return <div>Recipe CARD</div>;
+type RecipeCardProps = {
+  recipe: Recipe;
+};
+
+export function RecipeCard({ recipe }: RecipeCardProps) {
+  return (
+    <div>
+      <h3>{recipe.name}</h3>
+      <ul>
+        {recipe.ingredients.map((ingredient) => (
+          <li key={ingredient.id}>
+            {ingredient.name}: {ingredient.amount}
+            {""} {ingredient.unit}
+          </li>
+        ))}
+      </ul>
+      <p>Base Servings: {recipe.baseServings}</p>
+    </div>
+  );
 }
--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,5 +1,17 @@
-import { useState } from "react";
+import type { Recipe } from "../types";
+import { RecipeCard } from "./RecipeCard";
+type RecipeListProps = {
+  recipes: Recipe[];
+};
 
-export function RecipeList() {
-  return <div>Recipe List</div>;
+export function RecipeList({ recipes }: RecipeListProps) {
+  return (
+    <div className="">
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => <RecipeCard key={recipe.id} recipe={recipe} />)
+      ) : (
+        <p>No recipes added yet. Add your first recipe above. </p>
+      )}
+    </div>
+  );
 }
