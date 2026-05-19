67eca7cdbdf62608f5e57f04064cebacb864214a
desfurdev <desfurdev@gmail.com>
2026-05-19 10:26:11 -0400

    feat: add per-card serving counter

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,22 +1,39 @@
 import type { Recipe } from "../types";
-
+import { useState } from "react";
 type RecipeCardProps = {
   recipe: Recipe;
 };
 
 export function RecipeCard({ recipe }: RecipeCardProps) {
+  const [currentServings, setCurrentServings] = useState(recipe.baseServings);
+  const incrementServings = (): void => {
+    setCurrentServings((prev) => prev + 1);
+  };
+
+  const decrementServings = (): void => {
+    setCurrentServings((prev) => {
+      if (prev > 1) {
+        return prev - 1;
+      }
+      return 1;
+    });
+  };
   return (
     <div>
       <h3>{recipe.name}</h3>
       <ul>
         {recipe.ingredients.map((ingredient) => (
           <li key={ingredient.id}>
-            {ingredient.name}: {ingredient.amount}
-            {""} {ingredient.unit}
+            {ingredient.name}: {ingredient.amount} {ingredient.unit}
           </li>
         ))}
       </ul>
-      <p>Base Servings: {recipe.baseServings}</p>
+      <p>Base servings: {recipe.baseServings}</p>
+      <p>Current servings: {currentServings}</p>
+      <button onClick={decrementServings} disabled={currentServings <= 1}>
+        -
+      </button>
+      <button onClick={incrementServings}>+</button>
     </div>
   );
 }
